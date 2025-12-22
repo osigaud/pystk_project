225e46b812351ee3aaec447a98b9ad7cbbc6f1c3
osigaud <Olivier.Sigaud@upmc.fr>
2025-12-22 16:28:37 +0100

    leaderboard on one race now available

--- src/main/mono_race_display.py
+++ src/main/mono_race_display.py
@@ -2,13 +2,14 @@
 MultiAgent race
 
 All initial agents are RandomAgent
-The simulation runs on the "black_forest" track with 7 karts.
+The simulation runs on the "black_forest" track with MAX_TEAMS karts.
 """
 
 import sys, os
 import numpy as np
-
-MAX_TEAMS = 7
+from datetime import datetime
+from pathlib import Path
+from dataclasses import dataclass
 
 # Append the "src" folder to sys.path.
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "src")))
@@ -22,6 +23,20 @@ from agents.team6.agent6 import Agent6
 from agents.team7.agent7 import Agent7
 from pystk2_gymnasium.envs import STKRaceMultiEnv, AgentSpec
 
+MAX_TEAMS = 7
+
+# Get the current timestamp
+current_timestamp = datetime.now()
+
+# Format it into a human-readable string
+formatted_timestamp = current_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+@dataclass
+class Scores:
+    name: str
+    position: float
+    pos_std: float
+
 default_action = {
             "acceleration": 0.0,
             "steer": 0.0,
@@ -34,7 +49,7 @@ default_action = {
 
 # Make AgentSpec hashable.
 def agent_spec_hash(self):
-    return hash((self.rank_start, self.use_ai, self.name, self.camera_mode))
+    return hash((self.name, self.rank_start, self.use_ai, self.camera_mode))
 AgentSpec.__hash__ = agent_spec_hash
 
 # Create agents specifications.
@@ -43,7 +58,7 @@ agents_specs = [
 ]
 
 # Create the multi-agent environment for N karts.
-env = STKRaceMultiEnv(agents=agents_specs, track="xr591", render_mode="human", num_kart=7)
+env = STKRaceMultiEnv(agents=agents_specs, track="xr591", render_mode="human", num_kart=MAX_TEAMS)
 
 # Instantiate the agents.
 
@@ -67,7 +82,7 @@ def main():
     done = False
     steps = 0
     positions = []
-    while not done and steps < 100:
+    while not done and steps < 1000:
         actions = {}
         for i in range(MAX_TEAMS):
             str = f"{i}"
@@ -90,9 +105,56 @@ def main():
         positions.append(pos)
     average_pos = np.array(positions).mean(axis=0)
     std_pos = np.array(positions).std(axis=0)
+    scores = []
     for i in range(MAX_TEAMS):
-        print(f"{names[i]} avg:{average_pos[i]} std:{std_pos[i]}")
+        scores.append(Scores(names[i], average_pos[i], std_pos[i]))
     env.close()
+    return scores
+
+
+def output_html(output: Path, scores: Scores):
+    # Use https://github.com/tofsjonas/sortable?tab=readme-ov-file#1-link-to-jsdelivr
+    with output.open("wt") as fp:
+        fp.write(
+            f"""<html><head>
+<title>STK Race results</title>
+<link href="https://cdn.jsdelivr.net/gh/tofsjonas/sortable@latest/dist/sortable.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/gh/tofsjonas/sortable@latest/dist/sortable.min.js"></script>
+<body>
+<h1>Team evaluation on SuperTuxKart</h1><div style="margin: 10px; font-weight: bold">Timestamp: {formatted_timestamp}</div>
+<table class="sortable n-last asc">
+  <thead>
+    <tr>
+      <th class="no-sort">Name</th>
+      <th id="position">Avg. position</th>
+      <th class="no-sort">±</th>
+    </tr>
+  </thead>
+  <tbody>"""
+        )
+
+        for i in range(MAX_TEAMS):
+            fp.write(f"""<tr><td>{scores[i].name}</td>""")
+            fp.write(
+                    f"""<td>{scores[i].position:.2f}</td>"""
+                    f"""<td>{scores[i].pos_std:.2f}</td>"""
+                    "</tr>"
+                )
+        fp.write(
+            """<script>
+  window.addEventListener('load', function () {
+    const el = document.getElementById('position')
+    if (el) {
+      el.click()
+    }
+  })
+</script>
+"""
+        )
+        fp.write("""</body>""")
+
+
 
 if __name__ == "__main__":
-    main()
+    scores = main()
+    output_html(Path("./results.html"), scores)
