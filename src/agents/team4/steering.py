9eaff1c0c9caf5ede66f901dc16d88f168258d26
Adam <bouzou734@gmail.com>
2026-01-31 20:33:37 +0100

    recovery of x, the lateral shift

--- src/agents/team4/steering.py
+++ src/agents/team4/steering.py
@@ -1,3 +1,7 @@
 class Steering:
     def steering(self,obs):
-        
+        points = obs['paths_start'] #on recupère les noeuds
+
+        if len(points)>2:
+            x = points[2][0] #deballage du décalage latéral en prenant le troisième point sur la liste
+
