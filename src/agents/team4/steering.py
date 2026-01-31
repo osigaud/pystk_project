003f8898b6d9533fa434c533f74c46a787f46de8
Adam <bouzou734@gmail.com>
2026-01-31 20:57:37 +0100

    correction of the steering function

--- src/agents/team4/steering.py
+++ src/agents/team4/steering.py
@@ -1,8 +1,8 @@
 class Steering:
     def steering(self,obs):
         points = obs['paths_start'] #on recupère les noeuds
-
+        x = 0.0
         if len(points)>2:
             x = points[2][0] #deballage du décalage latéral en prenant le troisième point sur la liste
-            return x*0.5 #coefficient pour rendre l'agent moins nerveux
+        return x*0.7 #coefficient pour rendre l'agent moins nerveux
 
