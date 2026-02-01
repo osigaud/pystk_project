34982763d28a83ccf170b611d84dab05a38a4208
wiambsr <wiam.bensaber07@gmail.com>
2026-02-01 15:51:33 +0100

    Ajout de la classe AgentCenter qui reste à |0.5| du centre

--- src/agents/team1/agent1.py
+++ src/agents/team1/agent1.py
@@ -52,18 +52,27 @@ class AgentStraight(AgentBase):
 
 #Agent qui suit le centre de la piste
 #méthodes à rajouter ici
-class AgentCenter(AgentStraight) :
+class AgentCenter(KartAgent):
+    def __init__(self, env, dist, ajust, base_agent):
+        super().__init__(env)
+        self.base = base_agent
+        self.dist = dist
+        self.ajust = ajust
+
+    def path_ajust(self, act, obs, min_d, max_d):
+        s = act["steer"]
+        center = obs["center_path_distance"]
+        if center < min_d: 
+            s=s+self.ajust
+        elif center > max_d: 
+            s=s-self.ajust
+        act["steer"] = s
+        return act 
+    
     def choose_action(self, obs):
-        action = {
-            "acceleration": 1, 
-            "steer": 0,
-            "brake": False, 
-            "drift": False, 
-            "nitro": False, 
-            "rescue": False, 
-            "fire": False, 
-        }
-        return action
+            act = self.base.choose_action(obs)
+            act_corr = self.path_ajust(act, obs, -0.5, 0.5)
+            return act_corr
 
 #AGENT FINAL :
 class Agent1(AgentCenter):
