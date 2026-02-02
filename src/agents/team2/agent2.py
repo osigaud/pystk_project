02217f47efa49c615067b9a701bad44aa4579976
mariammm0 <mariam.abd_el_moneim@etu.sorbonne-universite.fr>
2026-02-02 21:41:14 +0100

    Ajout de la fonction _calcul_angle dans le fichier agent2

--- src/agents/team2/agent2.py
+++ src/agents/team2/agent2.py
@@ -21,16 +21,55 @@ class Agent2(KartAgent):
     def endOfTrack(self):
         return self.isEnd
 
+    #implémentation du code calcul d'angle 
+    def _calcul_angle(self,obs):
+        if len(obs) < 1 :  #permet de vérifier si obs existe 
+            return 0.0 
+        elif len(obs["paths_start"]) < 1:
+            return 0.0
+        elif len(obs["paths_end"]) < 1: # paths_end renvoie un segment de fin 
+            return 0.0
+        elif len(obs["karts_position"]) < 1 :
+            return 0.0
+        elif len(obs["velocity"]) < 3 : 
+            return 0.0
+        else : 
+            paths_start = obs["paths_start"] # x y z 
+            paths_end = obs["paths_end"] # x2 y2 z2 
+            karts_position = obs["karts_position"] 
+            #nodes = obs["path_nodes"] n existe pas 
+            
+            idx = min(self.path_lookahead, len(paths_start) - 1)
+
+            seg_start = np.array(paths_start[idx])
+            seg_end = np.array(paths_end[idx]) 
+            pos = np.array(karts_position[0])
+            
+            cible = (seg_start+seg_end)/2
+            vecteur_traj = cible - pos
+            
+            vecteur_vit = np.array(obs["velocity"][0:3]) #[0:3] -> on prend les coordonnées x y z 
+            
+            produit_vect = vecteur_traj[0]*vecteur_vit[2] - vecteur_traj[2]*vecteur_vit[0]
+            produit_scal = vecteur_traj[0]*vecteur_vit[0] + vecteur_traj[2]*vecteur_vit[2]
+            angle = np.arctan2(produit_vect,produit_scal)
+            
+            return angle             
+    
     def choose_action(self, obs):
-        acceleration = random.random()
-        steering = random.random()
+        angle = self._calcul_angle(obs)
+        print(f"Angle actuel: {angle:.3f} rad, {np.degrees(angle):.1f}°")
+
+        acceleration = 0
+        steering = -1.0
         action = {
             "acceleration": acceleration,
-            "steer": steering,
-            "brake": False, # bool(random.getrandbits(1)),
-            "drift": bool(random.getrandbits(1)),
-            "nitro": bool(random.getrandbits(1)),
-            "rescue":bool(random.getrandbits(1)),
-            "fire": bool(random.getrandbits(1)),
+            "steer":  False, #np.clip(angle, -0.5, 0.5),
+            "brake": False, #abs(angle) > 1.0, # bool(random.getrandbits(1)),
+            "drift": False, 
+            "nitro": False,
+            "rescue": False,
+            "fire": False,
+>>>>>>> cda782b (Ajout de la fonction _calcul_angle dans le fichier agent2)
         }
         return action
