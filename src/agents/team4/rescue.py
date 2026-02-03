7443418367d660e2b56cee9da3713b0f09bacadd
Adam <bouzou734@gmail.com>
2026-02-03 21:52:33 +0100

    Remove library not used

--- src/agents/team4/rescue.py
+++ src/agents/team4/rescue.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 class RescueManager:
     def __init__(self):
         self.agent_positions = []
@@ -9,12 +7,9 @@ class RescueManager:
         
         self.agent_positions.append(distance)
         
-        
         if len(self.agent_positions) >= 20 and distance > 10.0:
             
             delta = self.agent_positions[-1] - self.agent_positions[-7]
-
-            
             if abs(delta) < 3 and speed < 0.30:
                 self.times_blocked += 1
             else:
@@ -23,11 +18,9 @@ class RescueManager:
             if self.times_blocked > 14:
                 self.times_blocked = 0
         
-        
         return self.times_blocked >= 7
 
     def sortir_du_mur(self, current_steer):
-        
         
         recovery_steer = -1.0 if current_steer > 0 else 1.0
         
