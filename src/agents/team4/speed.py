d053107aaa365c6391280235e9dfcb9b40fb6e7a
Aines_93 <ainesbenomrane06@gmail.com>
2026-02-03 22:44:03 +0100

    creation d'une deuxieme fonction vitesse optimisée

--- src/agents/team4/speed.py
+++ src/agents/team4/speed.py
@@ -4,3 +4,9 @@ class SpeedController:
             return 0.3
         else:
             return 1.0
+    
+    def vitesse2(self, steering):
+        accel = 1.0 - (abs(steering) * 0.8)
+    
+    # Sécurité : on ne descend jamais sous 0.1 pour ne pas s'arrêter
+        return max(0.1, accel)
