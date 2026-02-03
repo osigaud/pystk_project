cbf8bcf1e900e6f23d307558331a84ac6700cd6e
Adam <bouzou734@gmail.com>
2026-02-03 19:30:23 +0100

    Adding the case where the list does not contain enough points

--- src/agents/team4/steering.py
+++ src/agents/team4/steering.py
@@ -10,11 +10,12 @@ class Steering:
     
     def steering_v2(self,obs):
         points = obs['paths_start'] #on récupère les noeuds
-        x = 0.0
         if len(points)>2:
-            x = points[2][0] #deballage du décalage latéral en prenant le deuxième point sur la liste
-            z = points[2][2] #deballage de la profondeur en prenant le deuxième point sur la liste
+            x = points[2][0] #deballage du décalage latéral en prenant le troisième point sur la liste
+            z = points[2][2] #deballage de la profondeur en prenant le troisième point sur la liste
             steer = math.atan2(x,z) #calcul de l'angle de braquage avec math.atan2 qui gère le cas où z vaut 0 ainsi que le signe de x et z
-            return steer
+        else:
+            steer = 0.0
+        return steer
 
 
