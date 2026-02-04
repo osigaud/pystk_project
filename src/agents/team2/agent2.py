4a76d8f2f7dca9962088a2d373ca51224b9f5977
Selma <selma@MacBook-Air-de-Selma.local>
2026-02-04 10:51:07 +0100

    ajout d'un debut de code pour reagir a un objet et creation de la fonction calculer_direction

--- src/agents/team2/agent2.py
+++ src/agents/team2/agent2.py
@@ -21,9 +21,22 @@ class Agent2(KartAgent):
     def endOfTrack(self):
         return self.isEnd
 
+
+def calculer_direction(self,obs ) : 
+        distance = obs['center_path'][0] # center_path designe le vecteur du centre de la route donc il contient 3 coordonnees l'un l'index gauche ou droite l'autre la hauteur et le dernier la distance devant nous le 0 donc c'est l'indice de ce quon a besoin poiur la distance 
+
+        angle = distance * 0.4 # si la distance est par exemple 2 metres  il va reagir que 40% de la distance 
+        if angle > 1:
+            angle = 1  # on s'assure que l'angle reste entre -1 et 1
+        elif angle < -1:
+            angle = -1
+            
+        return angle        
+
+
     def choose_action(self, obs):
-        acceleration = random.random()
-        steering = random.random()
+        acceleration = 1
+        steering = 0
         action = {
             "acceleration": acceleration,
             "steer": steering,
@@ -32,5 +45,32 @@ class Agent2(KartAgent):
             "nitro": bool(random.getrandbits(1)),
             "rescue":bool(random.getrandbits(1)),
             "fire": bool(random.getrandbits(1)),
+
+        #if target_item_distance== 10:
+            if obs['target_item_type'] in bad_type:
+                if target_item_angle >5: # obstacle a droite
+                    action["steer"]= - 0.5 # on tourne a gauche
+                    action["nitro"]=False
+                    action["acceleration"]= action["acceleration"] - 0.5 #val a determiner
+                elif target_item_angle ==0 : # obstacle a droite
+                    action["steer"]= - 0.5 # on tourne a gauche ou droite
+                    action["nitro"]=False
+                    action["acceleration"]= action["acceleration"] - 0.5 #val a determiner
+                elif target_item_angle <-5: # obstacle a gauche 
+                    action["steer"]= 0.5 # on tourne a droite
+                    action["nitro"]=False
+                    action["acceleration"]= action["acceleration"] - 0.5 #val a determiner
+
+            elif target_item_type in good_type:
+                if target_item_angle <-5: # a gauche 
+                    action["steer"]= - 0.5 # on tourne a gauche
+                    action["nitro"]=True
+                    action["acceleration"]= action["acceleration"] + 0.5 #val a determiner
+                elif target_item_angle >5: #a droite
+                    action["steer"]= 0.5 # on tourne a droite
+                    action["nitro"]=True
+                    action["acceleration"]= action["acceleration"] + 0.5 #val a determiner
+
+
         }
         return action
