37cfae7e03b917e762719fa1c9e08be30fd08a7f
Adam <bouzou734@gmail.com>
2026-02-07 19:58:03 +0100

    creation of the function pure_pursuit

--- src/agents/team4/steering.py
+++ src/agents/team4/steering.py
@@ -2,14 +2,7 @@ import math
 import numpy as np
 
 class Steering:
-    def manage_steering(self,obs):
-        points = obs['paths_start'] #on récupère les noeuds
-        if len(points)>2:
-            x = points[2][0] #deballage du décalage latéral en prenant le troisième point sur la liste
-            z = points[2][2] #deballage de la profondeur en prenant le troisième point sur la liste
-            steer = math.atan2(x,z) * 2 #calcul de l'angle de braquage avec math.atan2 qui gère le cas où z vaut 0 ainsi que le signe de x et z
-        else:
-            steer = 0.0
-        return np.clip(steer,-1,1) #ajout de la sécurite np.clip pour garder le steer entre -1 et 1
+    def manage_pure_pursuit(self,obs):
+        
 
 
