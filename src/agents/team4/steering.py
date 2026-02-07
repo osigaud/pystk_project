6524d2325535373d0e7fdf610742c971e8433f5b
Adam <bouzou734@gmail.com>
2026-02-07 19:50:18 +0100

    adding a coefficient to the steering

--- src/agents/team4/steering.py
+++ src/agents/team4/steering.py
@@ -6,7 +6,7 @@ class Steering:
         if len(points)>2:
             x = points[2][0] #deballage du décalage latéral en prenant le troisième point sur la liste
             z = points[2][2] #deballage de la profondeur en prenant le troisième point sur la liste
-            steer = math.atan2(x,z) #calcul de l'angle de braquage avec math.atan2 qui gère le cas où z vaut 0 ainsi que le signe de x et z
+            steer = math.atan2(x,z) * 2 #calcul de l'angle de braquage avec math.atan2 qui gère le cas où z vaut 0 ainsi que le signe de x et z
         else:
             steer = 0.0
         return steer
