ff0d12df1902a0a628d62967992289429d201786
Adam <bouzou734@gmail.com>
2026-02-07 20:04:23 +0100

    calculating the radius

--- src/agents/team4/steering.py
+++ src/agents/team4/steering.py
@@ -5,6 +5,13 @@ class Steering:
     def manage_pure_pursuit(self,obs):
         points = obs.get("paths_start",(0,0,0))
         target = points[2]
+        gx = target[0]
+        gz = target[2]
+        l2 = gx**2 + gz**2
+        rayon = l2/(2*gx)
+
+        
+
 
 
 
