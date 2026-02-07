4370a989706ca200453ab0cf19d5a91de6d6f81b
Adam <bouzou734@gmail.com>
2026-02-07 20:08:39 +0100

    calculation of the angle and addition of a security

--- src/agents/team4/steering.py
+++ src/agents/team4/steering.py
@@ -3,16 +3,24 @@ import numpy as np
 
 class Steering:
     def manage_pure_pursuit(self,obs):
+        
         points = obs.get("paths_start",(0,0,0))
-        target = points[2]
-        gx = target[0]
-        gz = target[2]
-        l2 = gx**2 + gz**2
-        rayon = l2/(2*gx)
+        if(len(points)>2): 
+            target = points[2]
+            gx = target[0]
+            gz = target[2]
+            l2 = gx**2 + gz**2
+            rayon = l2/(2*gx)
 
-        L = 2.5 # On simule un empattement
+            L = 2.5 # On simule un empattement
 
-        tan_angle = L/rayon
+            steer = math.atan2(L,rayon)
+
+            return np.clip(steer*4.5,-1,1)
+        else:
+            return 0.0
+
+        
 
 
 
