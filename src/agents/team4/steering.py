4b2d15a4a28dcae64939a1d972676f3f75a1e88d
Adam <bouzou734@gmail.com>
2026-02-07 20:19:50 +0100

    add comments

--- src/agents/team4/steering.py
+++ src/agents/team4/steering.py
@@ -6,18 +6,20 @@ class Steering:
         
         points = obs.get("paths_start",[])
         if len(points) > 2: 
-            target = points[2]
-            gx = target[0]
-            gz = target[2]
-            l2 = gx**2 + gz**2
+            target = points[2] # Recuperation du troisième point
+            gx = target[0] # Recuperation de x, le decalage lateral
+            gz = target[2] # Recuperation de z, la profondeur
+            l2 = gx**2 + gz**2 # calcul de l'hypoténuse
             
-            if l2 < 0.01 : return 0.0
+            if l2 < 0.01 : return 0.0 # Si on est déjà sur la cible, on ne tourne pass
             
             L = 2.5 # On simule un empattement
 
-            steer = math.atan2(2 * L * gx,l2)
+            angle = math.atan2(2 * L * gx,l2) # Calcul de la formule issu de pure_pursuit et du modèle bicyclette
 
-            return np.clip(steer*4.5,-1,1)
+            steer = angle * 4.5 # ajout d'un coefficient pour ramener sur notre referentiel
+
+            return np.clip(steer,-1,1) # Ajout d'une sécurité pour garder le steer entre -1 et 1
         else:
             return 0.0
 
