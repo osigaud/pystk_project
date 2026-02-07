eda6db6314e77e40f9d5c568323d282530e7e8b1
Adam <bouzou734@gmail.com>
2026-02-07 20:12:24 +0100

    added some adjustments to avoid crashes

--- src/agents/team4/steering.py
+++ src/agents/team4/steering.py
@@ -4,17 +4,18 @@ import numpy as np
 class Steering:
     def manage_pure_pursuit(self,obs):
         
-        points = obs.get("paths_start",(0,0,0))
-        if(len(points)>2): 
+        points = obs.get("paths_start",[])
+        if len(points) > 2: 
             target = points[2]
             gx = target[0]
             gz = target[2]
             l2 = gx**2 + gz**2
-            rayon = l2/(2*gx)
-
+            
+            if l2 < 0.01 : return 0.0
+            
             L = 2.5 # On simule un empattement
 
-            steer = math.atan2(L,rayon)
+            steer = math.atan2(2 * L * gx,l2)
 
             return np.clip(steer*4.5,-1,1)
         else:
