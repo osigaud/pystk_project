6b41cba15c3f023e885c1e952671b315e4a790b5
Adam <bouzou734@gmail.com>
2026-02-09 12:12:50 +0100

    integration of the Banana class into the Steering class

--- src/agents/team4/steering.py
+++ src/agents/team4/steering.py
@@ -1,11 +1,13 @@
 import math
 import numpy as np
+from .banana_detection import Banana
 
 class Steering:
     
     def __init__(self):
         self.L = 2.5  # On simule un empattement
         self.gain = 6.0 # ajout d'un coefficient pour ramener sur notre referentiel
+        self.banana_dodge = Banana()
     
     
     def manage_pure_pursuit(self,obs):
@@ -14,10 +16,12 @@ class Steering:
         
         if len(points) <= 2:return 0.0 
         
-        
         target = points[2] # Recuperation du troisième point
         gx = target[0] # Recuperation de x, le decalage lateral
         gz = target[2] # Recuperation de z, la profondeur
+        
+        danger, b_x = self.banana_dodge.banana_detection(obs)
+        
         l2 = gx**2 + gz**2 # calcul de l'hypoténuse
             
         if l2 < 0.01 : return 0.0 # Si on est déjà sur la cible, on ne tourne pass
