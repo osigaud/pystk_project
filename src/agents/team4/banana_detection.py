db0a19a7b8fcbae197a92dee66156c24f40ce5d9
Adam <bouzou734@gmail.com>
2026-02-09 12:09:51 +0100

    Realization of the detection logic

--- src/agents/team4/banana_detection.py
+++ src/agents/team4/banana_detection.py
@@ -4,3 +4,11 @@ class Banana:
 
         items_pos = obs['items_position']
         items_type = obs['items_type']
+
+        for i in range(len(items_pos)):
+            if items_type[i] == 1:
+                pos_x = items_pos[i][0]
+                pos_z = items_pos[i][2]
+                if -2.5 <= pos_x <= 2.5 and 0.1 <= pos_z <= 8.0:
+                    return True, pos_x
+        return False,0
