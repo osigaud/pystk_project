0125251db09f661f46a8be330ab3be8a48b8a1dc
Dylan <dylan.greedharry@etu.sorbonne-universite.fr>
2026-02-11 08:56:15 +0100

    Remove a useless line in our code.

--- src/agents/team3/agent3.py
+++ src/agents/team3/agent3.py
@@ -24,18 +24,17 @@ class Agent3(KartAgent):
         steer = (x / z) * 1.5
         items_pos = np.array(obs["items_position"])
         items_type = np.array(obs["items_type"])
-        if len(items_pos) > 0:
-            for i in range(len(items_pos)):
-                item_pos = items_pos[i]
-                item_type = items_type[i]
-                if (item_type == 1 or item_type == 4):
-                    item_x = item_pos[0]
-                    item_z = item_pos[2]
-                    if (0 < item_z < 30 and abs(item_x) < 3.0):
-                        if item_x <= 0:
-                            steer += 0.5
-                        else:
-                            steer -= 0.5
+        for i in range(len(items_pos)):
+            item_pos = items_pos[i]
+            item_type = items_type[i]
+            if (item_type == 1 or item_type == 4):
+                item_x = item_pos[0]
+                item_z = item_pos[2]
+                if (0 < item_z < 30 and abs(item_x) < 3.0):
+                    if item_x <= 0:
+                        steer += 0.5
+                    else:
+                        steer -= 0.5
         speed = (((obs["velocity"][0])**2) + ((obs["velocity"][2])**2))**0.5
         brake = False
         nitro = False
