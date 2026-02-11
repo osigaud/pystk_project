ba51d887d44bfdc209245ed3a8008b2a23f88956
Dylan <dylan.greedharry@etu.sorbonne-universite.fr>
2026-02-11 08:29:38 +0100

    Changed the logic of our choose_action function in order to make our kart more stable. Our kart only fire when he's in a straight line. Also changed the way we calculate the speed of our kart. Added np.array to optimize our code

--- src/agents/team3/agent3.py
+++ src/agents/team3/agent3.py
@@ -1,14 +1,13 @@
+import numpy as np
 from agents.kart_agent import KartAgent
-from agents.team3.FireItems import FireItems
 
 class Agent3(KartAgent):
     def __init__(self, env, path_lookahead=3):
         super().__init__(env)
         self.path_lookahead = path_lookahead
-        self.agent_positions = []
         self.obs = None
         self.isEnd = False
-        self.name = "TEAM L'ÉCLAIR"
+        self.name = "Team L'Eclair"
         self.time_blocked = 0
 
     def reset(self):
@@ -19,6 +18,37 @@ class Agent3(KartAgent):
         return self.isEnd
     
     def choose_action(self, obs):
-
-        action = FireItems.choose_action(self, obs)
+        target = np.array(obs["paths_start"][self.path_lookahead]) 
+        x = target[0]
+        z = target[2]
+        steer = (x / z) * 1.5
+        speed = (((obs["velocity"][0])**2) + ((obs["velocity"][2])**2))**0.5
+        brake = False
+        nitro = False
+        fire = False
+        if abs(steer) > 1.0:
+            acceleration = 0.3
+        else:
+            acceleration = 1.0
+        if abs(steer) < 0.1 and speed < 20:
+            nitro = True
+        if (speed < 2.0):
+            self.time_blocked += 1 
+            if (self.time_blocked > 10):
+                acceleration = 0.0
+                brake = True
+                steer = -steer
+        if (self.time_blocked == 20):
+            self.time_blocked = 0 
+        if (x<1.0):
+        	fire = True
+        action = {
+            "acceleration": acceleration,
+            "steer": steer,
+            "brake": brake,
+            "drift": False,
+            "nitro": nitro,
+            "rescue": False,
+            "fire": fire,
+        }
         return action
