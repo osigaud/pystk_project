12fccc3197b169c7121cd16b199d8418dbdda68b
Dylan <dylan.greedharry@etu.sorbonne-universite.fr>
2026-02-11 08:44:11 +0100

    Changed the way our kart dodge the banana peels. He can also dodge the bubblegum now.

--- src/agents/team3/agent3.py
+++ src/agents/team3/agent3.py
@@ -22,6 +22,20 @@ class Agent3(KartAgent):
         x = target[0]
         z = target[2]
         steer = (x / z) * 1.5
+        items_pos = np.array(obs["items_position"])
+        items_type = np.array(obs["items_type"])
+        if len(items_pos) > 0:
+            for i in range(len(items_pos)):
+                item_pos = items_pos[i]
+                item_type = items_type[i]
+                if (item_type == 1 or item_type == 4):
+                    item_x = item_pos[0]
+                    item_z = item_pos[2]
+                    if (0 < item_z < 30 and abs(item_x) < 3.0):
+                        if item_x <= 0:
+                            steer += 0.5
+                        else:
+                            steer -= 0.5
         speed = (((obs["velocity"][0])**2) + ((obs["velocity"][2])**2))**0.5
         brake = False
         nitro = False
@@ -30,7 +44,7 @@ class Agent3(KartAgent):
             acceleration = 0.3
         else:
             acceleration = 1.0
-        if abs(steer) < 0.1 and speed < 20:
+        if abs(steer) < 0.1 and speed < 20.0:
             nitro = True
         if (speed < 2.0):
             self.time_blocked += 1 
