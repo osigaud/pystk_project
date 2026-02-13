c5d1c34db8cea787aae1bef009a6e398b51aadb5
Adam <bouzou734@gmail.com>
2026-02-13 17:38:51 +0100

    modification of the signature of the manage_pure_pursuit function and passing of 'gain' as a parameter of the function

--- src/agents/team4/steering.py
+++ src/agents/team4/steering.py
@@ -5,33 +5,9 @@ class Steering:
     
     def __init__(self):
         self.L = 2.5  # On simule un empattement
-        self.gain = 6.0 # ajout d'un coefficient pour ramener sur notre referentiel
-    
-    def manage_pure_pursuit(self,obs):
         
-        points = obs.get("paths_start",[]) # On récupère les noeuds
+    def manage_pure_pursuit(self,gx,gz,gain):
         
-        if len(points) <= 2 : return 0.0 # Si la liste ne contient pas assez de points on renvoie un steer de 0
-        
-        target = points[2] # Recuperation du troisième point
-        gx = target[0] # Recuperation de x, le decalage lateral
-        gz = target[2] # Recuperation de z, la profondeur 
-
-        danger, b_x = self.banana_dodge.banana_detection(obs) # Appel de la fonction detection banane
-
-        if danger:
-            
-            target = points[1] # Changement de référentiel pour plus de nervosite
-            gx = target[0] # Recuperation de x, le decalage lateral
-            gz = target[2] # Recuperation de z, la profondeur
-
-            esquive = 0.8 # Variable d'esquive
-
-            if b_x >= 0:
-                gx-=esquive # Si la banane est à notre droite on va à gauche
-            else:
-                gx+=esquive # Si la banane est à notre gauche on va à droite
-
         """else:
             
             
@@ -82,6 +58,6 @@ class Steering:
             
         angle = math.atan2(2 * self.L * gx,l2) # Calcul de la formule issu de pure_pursuit et du modèle bicyclette
 
-        steer = angle * self.gain # application du coefficient
+        steer = angle * gain # application du coefficient
         
         return np.clip(steer,-1,1) # Ajout d'une sécurité pour garder le steer entre -1 et 1
