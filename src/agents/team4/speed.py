5be530b74740f44c42326eefd3b07c4281229e7c
Adam <bouzou734@gmail.com>
2026-02-14 20:01:31 +0100

    modification of speed

--- src/agents/team4/speed.py
+++ src/agents/team4/speed.py
@@ -1,13 +1,42 @@
 class SpeedController:
     
-    def manage_speed(self, steer, distance):
-        if distance < 5.0:
+    def manage_speed(self, steer, obs):
+        
+        points = obs.get("paths_start",[])
+        vel = obs.get("velocity", [0.0, 0.0, 0.0])
+        speed = float(vel[2])
+
+        if len(points) <=2:
             return 1.0, False
-        a = abs(steer)
-        if a > 0.85:
-            return 0.35, True
-        if a > 0.65:
-            return 0.65, False
-        if a > 0.45:
-            return 0.85, False
-        return 1.0, False
+        
+
+        if obs['distance_down_track']<=2:
+            return 1.0, False
+        
+        i2 = 2
+        i3 = min(3,len(points)-1)
+        i4 = min(4,len(points)-1)
+        
+        target_now = points[i2][0]
+        target_soon = points[i3][0]
+        target_late = points[i4][0]
+
+        if target_now >= 12:
+            
+            if speed >=20.0:
+                #print("Gros Freinage !")
+                brake = True
+                acceleration = 0.0
+            else:
+                #print("On lache seuleument l'accelerateur !")
+                brake = False
+                acceleration = 0.0
+        
+        elif abs(steer) >= 0.7:
+            #print("En virage")
+            brake = False
+            acceleration = 0.8
+        else:
+            brake = False
+            acceleration = 1.0
+        return acceleration, brake
