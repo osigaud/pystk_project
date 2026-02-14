42c4d345b2b01e17e7c8d3d2c35bd7c97902b9d5
Adam <bouzou734@gmail.com>
2026-02-14 19:59:59 +0100

    modification of nitro

--- src/agents/team4/nitro.py
+++ src/agents/team4/nitro.py
@@ -1,8 +1,16 @@
 class Nitro:
     
-    def manage_nitro(self, steer, energy):
+    def manage_nitro(self, steer, energy,obs):
+        points = obs['paths_start']
+        
+
+        target_now = points[2][0]
+        target_soon = points[3][0]
+        target_late = points[4][0]
+        
+        
         nit = False
-        if (energy > 0.5 and abs(steer) < 0.45):
+        if (energy > 0.5 and abs(steer) < 0.45 and abs(target_now)<=5 and abs(target_soon) <= 5 and target_late <= 7):
             nit = True
         return nit
 
