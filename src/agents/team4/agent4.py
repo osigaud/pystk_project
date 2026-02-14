afc7b109222b7cd58aa4a08eca2bdbe0704414ce
Adam <bouzou734@gmail.com>
2026-02-14 20:07:34 +0100

    modification of agent

--- src/agents/team4/agent4.py
+++ src/agents/team4/agent4.py
@@ -6,7 +6,8 @@ from agents.kart_agent import KartAgent
 from .steering import Steering
 from .rescue import RescueManager
 from .speed import SpeedController
-from .nitrodrift import NitroDrift
+from .nitro import Nitro
+from .drift import Drift
 from .banana_detection import Banana
 
 
@@ -21,8 +22,9 @@ class Agent4(KartAgent):
         self.steering = Steering()
         self.rescue = RescueManager()
         self.SpeedController=SpeedController()
-        self.nitrodrift = NitroDrift()
-        self.drift_cd = 0
+        self.nitro = Nitro()
+        self.drift = Drift()
+        self.drift_cd = -1
         self.banana_dodge = Banana()
         self.dodge_side = 0
         self.dodge_timer = 0
@@ -31,10 +33,11 @@ class Agent4(KartAgent):
     def reset(self):
         self.obs, _ = self.env.reset()
         self.agent_positions = []
-        self.drift_cd = 0
+        self.drift_cd = -1
         self.dodge_timer = 0
         self.dodge_side = 0
         self.last_banana_z = float("inf")
+        self.rescue = RescueManager()
 
     def endOfTrack(self):
         return self.isEnd
@@ -96,23 +99,45 @@ class Agent4(KartAgent):
         else:
             steering = self.steering.manage_pure_pursuit(gx,gz,6.0) # Appel à la fonction pure_pursuit en condition normale (pas de danger detecté)
         
+        epsilon = 0.05
+        
+        road_straight = abs(points[2][0]) < 0.8
+
+        if road_straight and abs(steering) <= epsilon:
+            steering = 0.0
+        
+        if obs['distance_down_track'] <= 2:
+            steering = 0.0
+
         distance = float(obs.get("distance_down_track", [0.0])[0])
         vel = obs.get("velocity", [0.0, 0.0, 0.0])
         speed = float(vel[2])
         energy = float(obs.get("energy", [0.0])[0])
-        """if self.drift_cd > 0:
+        drift = False
+        """
+        if not danger and not is_dodging:    
+            should_drift,drift_steer = self.drift.manage_drift(steering,distance)
+            if should_drift and self.drift_cd == -1:
+                self.drift_cd = 8
+                drift = True
+                
+            elif self.drift_cd > 0:
+                self.drift_cd -= 1
+                drift = True
+            else:
+                self.drift_cd = -1
+                drift = False
+        else :
             drift = False
-            self.drift_cd -= 1
-        else:
-            drift, steering = self.nitrodrift.manage_drift(steering, distance)
-            if drift:
-                self.drift_cd = 12"""
-        brea = False
-        acceleration, brea = self.SpeedController.manage_speed(steering,distance)
-
-
-
-        nitro = self.nitrodrift.manage_nitro(steering, energy)
+            self.drift_cd = -1"""
+        
+        
+        brake = False
+        acceleration, brake = self.SpeedController.manage_speed(steering,obs)
+        #print("speed_out:", self.SpeedController.manage_speed(steering, obs))
+        
+        nitro = False
+        nitro = self.nitro.manage_nitro(steering,energy,obs)
         """if (drift == True):
             nitro = False"""
 
@@ -121,16 +146,15 @@ class Agent4(KartAgent):
             return self.rescue.sortir_du_mur(steering)
         
         if is_dodging: # Si on est en train d'esquiver
-            acceleration = 0.8  # Baisse Accel
+            
             drift = False       # Pas de drift
             nitro = False       # Pas de nitro
-            brea = False        # Pas de break
         
         action = {
             "acceleration": acceleration,
             "steer": steering,
-            "brake": brea,
-            "drift": False,
+            "brake": brake,
+            "drift": drift,
             "nitro": nitro,
             "rescue":False,
             "fire": False,
