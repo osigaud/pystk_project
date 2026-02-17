d5337ab0587ccf44c6a211687d27f2a519a70419
Aines_93 <ainesbenomrane06@gmail.com>
2026-02-17 18:58:07 +0100

    ajout de la class esquive_adv

---
+++ src/agents/team4/esquive_adv.py
@@ -0,0 +1,15 @@
+class esquiveadv:
+    
+    def esquive_adv(self,obs):
+
+        kart_pos = obs.get('karts_position', []) 
+        if len(kart_pos) == 0:
+            return False, 0.0, 0.0
+
+        adv=kart_pos[0]
+        devant=False
+        if -0.8 <= adv[0] <=0.8 and  0.1<= adv[2]<=1.3:
+            devant=True
+
+        return devant,adv[0],adv[2]
+    
