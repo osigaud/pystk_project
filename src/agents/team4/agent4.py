3ff7613a8add55bc1a3e99f6ec891d4ea7f45390
mnasimi <mnasimi>
2026-02-17 09:08:09 +0100

    improvment of drift and speed

--- src/agents/team4/agent4.py
+++ src/agents/team4/agent4.py
@@ -10,7 +10,7 @@ from .nitrodrift import NitroDrift
 
 
 class Agent4(KartAgent):
-    def __init__(self, env, path_lookahead=3):
+    def __init__(self, env, path_lookahead=5):
         super().__init__(env)
         self.path_lookahead = path_lookahead
         self.agent_positions = []
@@ -22,6 +22,7 @@ class Agent4(KartAgent):
         self.SpeedController=SpeedController()
         self.nitrodrift = NitroDrift()
         self.drift_cd = 0
+        self.during_drift = 0
 
     def reset(self):
         self.obs, _ = self.env.reset()
@@ -35,20 +36,26 @@ class Agent4(KartAgent):
         
         steering = self.steering.manage_pure_pursuit(obs)
         distance = float(obs.get("distance_down_track", [0.0])[0])
-        vel = obs.get("velocity", [0.0, 0.0, 0.0])
-        speed = float(vel[2])
+        v = obs.get("velocity", [0.0, 0.0, 0.0])
+        speed = float((v[0]*v[0] + v[2]*v[2]) ** 0.5)
         energy = float(obs.get("energy", [0.0])[0])
+
         if self.drift_cd > 0:
             drift = False
             self.drift_cd -= 1
         else:
             drift, steering = self.nitrodrift.manage_drift(steering, distance)
             if drift:
-                self.drift_cd = 12
+                self.during_drift += 1
+            else :
+                self.during_drift = 0
+            
+            if self.during_drift > 3:
+                self.drift_cd = 7
+                self.during_drift = 0
         brea = False
-        acceleration, brea = self.SpeedController.manage_speed(steering,distance)
-
-
+        acceleration, brea = self.SpeedController.manage_speed(steering,speed,drift)
+        #print("The winners : ", speed)
 
         nitro = self.nitrodrift.manage_nitro(steering, energy)
         if (drift == True):
--- src/agents/team4/banana_detection.py
+++ src/agents/team4/banana_detection.py
@@ -6,7 +6,7 @@ class Banana:
         items_type = obs['items_type'] # Récupération des types des items 
 
         for i in range(len(items_pos)): #Boucle pour parcourir la liste entière des items
-            if items_type[i] == 1: #Si c'est une banane 
+            if items_type[i] == 1 or items_type[i] == 4: #Si c'est une banane 
                 pos_x = items_pos[i][0] # On récupère le décalage latéral
                 pos_z = items_pos[i][2] # On récupère la profondeur
                 if -2.5 <= pos_x <= 2.5 and 0.1 <= pos_z <= 8.0 and obs['distance_down_track'] >= 55.0:
