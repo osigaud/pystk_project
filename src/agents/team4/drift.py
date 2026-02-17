27ebea85f79ab4831c605203c0d5efc441f804d6
mnasimi <mnasimi>
2026-02-17 10:26:37 +0100

    Improve drift and integration in agent

--- src/agents/team4/drift.py
+++ src/agents/team4/drift.py
@@ -1,11 +1,8 @@
 class Drift:    
     
     def manage_drift(self, steer, distance):
-        if abs(steer) > 0.80 and distance > 5.0:
-            if steer > 0:
-                steer = steer + 0.12
-            else:
-                steer = steer - 0.12
+        if abs(steer) > 0.65 and distance > 5.0:
+            
     
             if steer > 1.0:
                 steer = 1.0
