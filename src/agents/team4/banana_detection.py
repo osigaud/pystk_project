9faba9e5ca45aaf50c9aa084ccadd3bddab2dce3
Adam <bouzou734@gmail.com>
2026-02-21 17:26:18 +0100

    Add comments in banana_detection

--- src/agents/team4/banana_detection.py
+++ src/agents/team4/banana_detection.py
@@ -5,38 +5,38 @@ class Banana:
         items_pos = obs['items_position'] # Récupération des positions des items
         items_type = obs['items_type'] # Récupération des types des items
 
-        banana = []
+        banana = [] # Liste qui va accueillir nos bananes
 
         for i in range(len(items_pos)):
-            if items_type[i] == 1 or items_type[i] == 4:
-                pos_x = items_pos[i][0]
-                pos_z = items_pos[i][2]
+            if items_type[i] == 1 or items_type[i] == 4: # Si c'est une banane ou une chewing-gum
+                pos_x = items_pos[i][0] # On récupère le décalage latéral 
+                pos_z = items_pos[i][2] # On récupère la profondeur
 
-                dist_obj_centre= abs(center_path+pos_x)
+                dist_obj_centre= abs(center_path+pos_x) #Calcul de la distance absolu de l'objet
 
-                if dist_obj_centre > limit_path:
+                if dist_obj_centre > limit_path: # Si l'objet est hors des limites de la piste, on ne le prend pas en compte
                     continue
 
-                if -2.0 <= pos_x <= 2.0 and 0.0 <= pos_z <= 17.0:
+                if -2.0 <= pos_x <= 2.0 and 0.0 <= pos_z <= 17.0: # Si la banana est dans notre radar, on l'ajoute dans notre liste
                     banana.append((pos_x,pos_z))
 
-        banana.sort(key=lambda x: x[1])
+        banana.sort(key=lambda x: x[1]) # On trie la liste par ordre croissant selon la profondeur
 
-        if len(banana) == 0:
+        if len(banana) == 0: # Rien à signaler
             return "CLEAR",None,None
         
-        first = banana[0]
+        first = banana[0] # On récupère la banane la plus proche
         first_x = first[0]
         first_z = first[1]
 
-        if len(banana) >=2:
-            second = banana[1]
+        if len(banana) >=2: #Si on a plus de deux bananes dans notre liste
+            second = banana[1] # On récupère la seconde
             x = second[0]
             z = second[1]
-            if abs(z-first_z) <= 2.0:
+            if abs(z-first_z) <= 2.0: # Si les bananes forment une ligne (un barrage)
                 gap_x = (x+first_x)/2.0
                 return "LIGNE", gap_x, banana
             else:
-                return "SINGLE", first_x,banana
+                return "SINGLE", first_x,banana # On revient sur le cas de la banane seule
         else:
-            return "SINGLE",first_x,banana
+            return "SINGLE",first_x,banana # Cas d'une seule banane
