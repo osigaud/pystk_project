dfb180e8fe4d02d0f6e3e5d693bd690dda074edf
IlyesTrahi1 <ilyestrahi@gmail.com>
2026-03-15 16:39:43 +0100

    rennommage variables agent_item

--- src/agents/team1/agent_items.py
+++ src/agents/team1/agent_items.py
@@ -1,5 +1,15 @@
 from agents.kart_agent import KartAgent
 import numpy as np
+case 1 : BUBBLEMGUM
+case 2 : CAKE
+case 3 : BOWLING
+case 4 : ZIPPER
+case 5 : PLUNGER
+case 6 : SWITCH
+case 7 : SWATTER
+case 8 : RUBBERBALL
+case 9 : PARACHUTE
+
 
 class AgentItems(KartAgent) : 
     def __init__(self, env, conf, agent) :
@@ -16,87 +26,84 @@ class AgentItems(KartAgent) :
         action["fire"] = False        
 
         match current_item : 
-            case 0 | 10 : 
-            #NOTHING ou ANVIL
-                return action
 
-            case 1 : 
+        if current_item == BUBBLEGUM :
             #BUBBLEGUM : à compléter
-                action["fire"] = True
-                return action
+            action["fire"] = True
+            return action
 
-            case 2 : 
+        if current_item == CAKE :
             #CAKE : à compléter
-                premier_kart = obs["karts_position"][0]
-                if premier_kart[2]<0:
-                    action["fire"] = False
-                    return action 
-                for kart in obs["karts_position"]:
-                    if kart[2]>=0 and kart[2]<60:
-                       action["fire"] = True
-                       return action
-                return action
+            premier_kart = obs["karts_position"][0]
+            if premier_kart[2]<0:
+                action["fire"] = False
+                return action 
+            for kart in obs["karts_position"]:
+                if kart[2]>=0 and kart[2]<60:
+                   action["fire"] = True
+                   return action
+            return action
             
-            case 3 : 
+        if current_item == BOWLING :
             #BOWLING : à compléter
-                premier_kart = obs["karts_position"][0]
-                if premier_kart[2]<0:
-                    action["fire"] = False
-                if self.target_item != None : 
-                    action ["fire"] = True
-                for kart in obs["karts_position"]:
-                    if kart[2]>=0 and kart[2]<=25:         #optimiser valeurs
-                        if abs(kart[0]) <= 3:
-                            action["fire"] = True                
-                return action
+            premier_kart = obs["karts_position"][0]
+            if premier_kart[2]<0:
+                action["fire"] = False
+            if self.target_item != None : 
+                action ["fire"] = True
+            for kart in obs["karts_position"]:
+                if kart[2]>=0 and kart[2]<=25:         #optimiser valeurs
+                    if abs(kart[0]) <= 3:
+                        action["fire"] = True                
+            return action
 
-            case 4 : 
+        if current_item == ZIPPER : 
             #ZIPPER : à compléter
-                #if virage_serre = false and self.target_obstacle = None:
-                action["fire"] = True               
-                return action
+            #if virage_serre = false and self.target_obstacle = None:
+            action["fire"] = True         	      
+            return action
 
-            case 5 : 
+        if current_item == PLUNGER :
             #PLUNGER : à compléter
-                premier_kart = obs["karts_position"][0]
-                if premier_kart[2]<0:
-                    action["fire"] = False
-                for kart in obs["karts_position"]:
-                    if kart[2]>=0 and kart[2]<25:
-                        if abs(kart[0]) <= 3:
-                            action["fire"] = True 
-                return action
+            premier_kart = obs["karts_position"][0]
+            if premier_kart[2]<0:
+                action["fire"] = False
+            for kart in obs["karts_position"]:
+                if kart[2]>=0 and kart[2]<25:
+                    if abs(kart[0]) <= 3:
+                        action["fire"] = True 
+            return action
 
-            case 6 : 
+        if current_item == SWITCH :
             #SWITCH : à compléter
-                if self.target_item != None:
-                    action["fire"] = False
-                else: 
-                    action["fire"] = True
-                return action
+            if self.target_item != None:
+                action["fire"] = False
+            else: 
+                action["fire"] = True
+            return action
 
-            case 7 : 
+        if current_item == SWATTER :
             #SWATTER : à compléter
-                for kart in obs["karts_position"]:
-                    if abs(kart[2])<=10 and abs(kart[0])<=10 and abs(kart[1])<=5:
-                        action["fire"] = True
-                return action
+            for kart in obs["karts_position"]:
+                if abs(kart[2])<=10 and abs(kart[0])<=10 and abs(kart[1])<=5:
+                    action["fire"] = True
+            return action
 
-            case 8 : 
+        if current_item == RUBBERBALL :
             #RUBBERBALL : à compléter
-                premier_kart = obs["karts_position"][0]
-                if premier_kart[2] > 0:
-                    action["fire"] = True
-                return action
+            premier_kart = obs["karts_position"][0]
+            if premier_kart[2] > 0:
+                action["fire"] = True
+            return action
 
-            case 9 : 
+        if current_item == PARACHUTE :
             #PARACHUTE : à compléter
-                premier_kart = obs["karts_position"][0]
-                if premier_kart[2] > 0:
-                    action["fire"] = True
-                return action
+            premier_kart = obs["karts_position"][0]
+            if premier_kart[2] > 0:
+                action["fire"] = True
+            return action
 
-        return action
+    return action
 
     def use_nitro(self, obs, act):
         nit = obs["energy"]
