0ca3608a0d4ccec1bc2cf1cc9a1fa8df76732679
maty <matyniang602@gmail.com>
2026-03-17 09:22:51 +0100

    modification du fichier optimise_kart.py

--- src/agents/team2/optimise_kart.py
+++ src/agents/team2/optimise_kart.py
@@ -7,7 +7,7 @@ def objective(trial):
     correction = trial.suggest_float("correction", 0.1, 1.0)
     curvature = trial.suggest_float("curvature", 0.1, 0.8)
 
-    chemin_yaml = "src/agents/team2/configDemoPilote.yaml"
+    chemin_yaml = "configDemoPilote.yaml"
 
     #ouverture de notre fichier de configuration 
     with open(chemin_yaml, 'r') as fichier:
@@ -26,11 +26,13 @@ def objective(trial):
     resultat = subprocess.run(
         ["uv", "run", "multi_track_race_display.py"], 
         capture_output=True, #Ça capture tout le texte que le jeu affiche à la fin
-        text=True
+        text=True,
+        cwd="../../main"
+        
     )
 
 
-
+    
 
     
     #on met un chrono de base de 300
@@ -53,7 +55,7 @@ def objective(trial):
 
 if __name__ == "__main__":
     etude = optuna.create_study(direction="minimize") #le but absolu c'est de trouver le resultat le plus petit possible 
-    #on lance 10 essai (trial)
+    #on lance 10 essais (trial)
     etude.optimize(objective, n_trials=10)
     print("Le meilleur temps obtenu est :", etude.best_value)
     print(" Les meilleurs réglages pour ton YAML sont :")
