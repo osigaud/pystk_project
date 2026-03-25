1f277998656f0c729cd9224afc0a8f6d8087d2e2
mysydiouf <mysyd@mysyvivobook.localdomain>
2026-03-25 02:20:47 +0100

    adapter les commentaire de acceleration_kart.py pour le doxygen

--- src/agents/team2/acceleration_kart.py
+++ src/agents/team2/acceleration_kart.py
@@ -12,10 +12,12 @@ from .anticipe_kart import AnticipeKart
 #
 #  Appelle AnticipeKart.detectVirage() pour mesurer la courbure courante,
 #  puis retourne une accélération réduite selon le type de virage :
-#  - Virage très serré (> cfg.virages.drift)        : 0.80
-#  - Virage serré      (cfg.virages.serrer.i1 à i2) : 0.85
-#  - Virage moyen      (cfg.virages.moyen.i1 à i2)  : 0.95
-#  - Ligne droite      (courbure faible)             : 1.00
+#  - Virage très serré (> cfg.virages.drift)        : cfg.acceleration.virage_tres_serré
+#  - Virage serré      (cfg.virages.serrer.i1 à i2) : cfg.acceleration.virage_serré
+#  - Virage moyen      (cfg.virages.moyen.i1 à i2)  : cfg.acceleration.virage_moyen
+#  - Ligne droite      (courbure faible)             : 1.0
+#
+#  Les valeurs d'accélération sont optimisées automatiquement par Optuna.
 #
 #  @see AnticipeKart
 class AccelerationControl(AnticipeKart):
@@ -23,8 +25,8 @@ class AccelerationControl(AnticipeKart):
     ## @brief   Initialise les seuils de classification des virages.
     #
     #  @param   cfg  Configuration OmegaConf issue de configDemoPilote.yaml.
-    #                Utilise les clés virages.drift, virages.serrer.i1/i2
-    #                et virages.moyen.i1/i2.
+    #                Utilise les clés virages.drift, virages.serrer.i1/i2,
+    #                virages.moyen.i1/i2 et acceleration.virage_*.
     def __init__(self, cfg):
         super().__init__()
 
@@ -47,12 +49,23 @@ class AccelerationControl(AnticipeKart):
         ## @var moyeni2
         #  @brief Borne supérieure de l'intervalle des virages moyens.
         self.moyeni2 = cfg.virages.moyen.i2
-        
-        # configuration des valeurs attribuées aux attributs d'instance
+
+        ## @var amax
+        #  @brief Accélération maximale appliquée en ligne droite (toujours 1.0).
         self.amax = 1.0
+
+        ## @var virage_tres_serré
+        #  @brief Accélération appliquée lors d'un virage très serré, optimisée par Optuna.
         self.virage_tres_serré = cfg.acceleration.virage_tres_serré
+
+        ## @var virage_serré
+        #  @brief Accélération appliquée lors d'un virage serré, optimisée par Optuna.
         self.virage_serré = cfg.acceleration.virage_serré
+
+        ## @var virage_moyen
+        #  @brief Accélération appliquée lors d'un virage moyen, optimisée par Optuna.
         self.virage_moyen = cfg.acceleration.virage_moyen
+
     ## @brief   Retourne l'accélération adaptée à la situation courante.
     #
     #  @param   obs  Dictionnaire d'observation retourné par l'environnement.
@@ -60,22 +73,15 @@ class AccelerationControl(AnticipeKart):
     #  @see     AnticipeKart.detectVirage()
     def adapteAcceleration(self, obs):
         acceleration = self.amax
-        #print("ancien : ", self.path_lookahead)
-        #self.path_lookahead = self.get_dynamicLookahead(obs)
-        curvature = abs(self.detectVirage(obs))  # valeur absolue de l'angle
-        #print("nouveau :", self.path_lookahead)
+        curvature = abs(self.detectVirage(obs))
 
         if curvature > self.seuildrift:
-            # virage très serré : fort freinage anticipé
-            #if self.virage_long :  #en fonction d un virage plus long on reduit l acceleration bcp plus 
-            #    acceleration = 0.65
-            #else : 
             acceleration = self.virage_tres_serré
-        elif curvature > self.serreri1 and curvature <= self.serreri2:  # virage serré
+        elif curvature > self.serreri1 and curvature <= self.serreri2:
             acceleration = self.virage_serré
-        elif curvature > self.moyeni1 and curvature <= self.moyeni2:    # virage moyen
+        elif curvature > self.moyeni1 and curvature <= self.moyeni2:
             acceleration = self.virage_moyen
         else:
-            # ligne droite ou courbe très légère : pleine accélération
             acceleration = self.amax
+
         return acceleration
