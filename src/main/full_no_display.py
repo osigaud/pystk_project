bafcaa0232bc751f9f646c2b584b0ea4b2602ad9
osigaud <Olivier.Sigaud@upmc.fr>
2026-03-27 12:48:38 +0100

    enriched scores, cleaned code

--- src/main/full_no_display.py
+++ src/main/full_no_display.py
@@ -4,7 +4,6 @@ MultiAgent race
 All initial agents are RandomAgent
 The simulation runs on the "black_forest" track with MAX_TEAMS karts.
 """
-
 import sys, os
 import numpy as np
 from datetime import datetime
@@ -18,20 +17,35 @@ from agents.team2.agent2 import Agent2
 from agents.team3.agent3 import Agent3
 from agents.team4.agent4 import Agent4
 from agents.team5.agent5 import Agent5
-from agents.team6.agent6 import Agent6
-from agents.team7.agent7 import Agent7
+from agents.hidden.median_agent import MedianAgent
+from agents.hidden.euler_agent import EulerAgent
+from agents.hidden.items_agent import ItemsAgent
 from pystk2_gymnasium.envs import STKRaceMultiEnv, AgentSpec
 from pystk2_gymnasium.definitions import CameraMode
 
-MAX_TEAMS = 7
-MAX_STEPS = 1000
-NB_RACES = 1
+from scores import Scores, output_html
+
+MAX_TEAMS = 5
+MAPS = ['abyss', 'black_forest', 'candela_city', 'cocoa_temple', 'cornfield_crossing', 'fortmagma', 'gran_paradiso_island', 'hacienda', 'lighthouse', 'mines', 'minigolf', 'olivermath', 'ravenbridge_mansion', 'sandtrack', 'scotland', 'snowmountain', 'snowtuxpeak', 'stk_enterprise', 'volcano_island', 'xr591', 'zengarden']
+MAX_STEPS = 2000
+NB_REPEAT = 2
 
 # Get the current timestamp
 current_timestamp = datetime.now()
 
 # Format it into a human-readable string
 formatted_timestamp = current_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+default_action = {
+            "acceleration": 0.0,
+            "steer": 0.0,
+            "brake": False, # bool(random.getrandbits(1)),
+            "drift": False, # bool(random.getrandbits(1)),
+            "nitro": False, # bool(random.getrandbits(1)),
+            "rescue":False, # bool(random.getrandbits(1)),
+            "fire": False, # bool(random.getrandbits(1)),
+        }
+
 
 # Make AgentSpec hashable.
 def agent_spec_hash(self):
@@ -43,9 +57,8 @@ agents_specs = [
     AgentSpec(name=f"Team{i+1}", rank_start=i, use_ai=False, camera_mode=CameraMode.ON) for i in range(MAX_TEAMS)
 ]
 
-def create_race():
-    # Create the multi-agent environment for N karts.
-    env = STKRaceMultiEnv(agents=agents_specs, track="abyss", render_mode="human", num_kart=MAX_TEAMS)
+def create_race(map=None):
+    env = STKRaceMultiEnv(agents=agents_specs, track=map, num_kart=MAX_TEAMS) #, render_mode="human"
 
     # Instantiate the agents.
 
@@ -57,8 +70,6 @@ def create_race():
     agents.append(Agent3(env, path_lookahead=3))
     agents.append(Agent4(env, path_lookahead=3))
     agents.append(Agent5(env, path_lookahead=3))
-    agents.append(Agent6(env, path_lookahead=3))
-    agents.append(Agent7(env, path_lookahead=3))
     np.random.shuffle(agents)
 
     for i in range(MAX_TEAMS):
@@ -68,12 +79,17 @@ def create_race():
     return env, agents, names
 
 
-def single_race(env, agents, names):
+def single_race(env, agents, names, scores):
     obs, _ = env.reset()
     done = False
     steps = 0
     nb_finished = 0
+    positions = []
     first = True
+    wins = np.zeros(MAX_TEAMS)
+    blocked = np.zeros(MAX_TEAMS)
+    for i in range(MAX_TEAMS):
+        agents[i].steps = MAX_STEPS
     while not done and steps < MAX_STEPS:
         actions = {}
         env.world_update()
@@ -82,16 +98,18 @@ def single_race(env, agents, names):
             try:
                 actions[str] = agents[i].choose_action(obs[str])
             except Exception as e:
-                print(f" {names[i]} error: {e}")
+                print(f"{names[i]} error: {e}")
                 actions[str] = default_action
 
             # check if agents have finished the race
             kart = env.world.karts[i]
             if kart.has_finished_race and not agents[i].isEnd:
-                print(f"{names[i]} has finished the race at step: {steps}")
+                print(f"{names[i]} has finished the race at step {steps}")
                 nb_finished += 1
                 agents[i].isEnd = True
+                agents[i].steps = steps
                 if first:
+                    wins[i] = 1
                     print(f"{names[i]} is first")
                 first = False
 
@@ -99,17 +117,40 @@ def single_race(env, agents, names):
 
         # prepare data to display leaderboard
         pos = np.zeros(MAX_TEAMS)
-        dist = np.zeros(MAX_TEAMS)
+        for i in range(MAX_TEAMS):
+            str = f"{i}"
+            pos[i] = info['infos'][str]['position']
         steps = steps + 1
         done = (nb_finished == MAX_TEAMS)
+        positions.append(pos)
+    pos_avg = np.array(positions).mean(axis=0)
+    pos_std = np.array(positions).std(axis=0)
+    for i in range(MAX_TEAMS):
+        if agents[i].steps == MAX_STEPS:
+            blocked[i] = 1
+            print(f"{names[i]} is blocked")
+    for i in range(MAX_TEAMS):
+        scores.append(names[i], pos_avg[i], pos_std[i], agents[i].steps, wins[i], blocked[i])
+        agents[i].isEnd = False
+    print("race duration:", steps)
 
 def main_loop():
+    scores = Scores()
     #unsatisfactory: first call just to init the names
     env, agents, names = create_race()
-    env, agents, names = create_race()
-    single_race(env, agents, names)
-    env.close()
+    for i in range(MAX_TEAMS):
+        scores.init(names[i])
+
+    for _ in range(NB_REPEAT):
+        for j in range(len(MAPS)):
+            track = MAPS[j]
+            print(f"race : {j} track : {track}")
+            env, agents, names = create_race(track)
+            single_race(env, agents, names, scores)
+            env.close()
+
+    return scores
         
 if __name__ == "__main__":
-    main_loop()
-
+    scores = main_loop()
+    output_html(Path("../../docs/index.html"), scores)
