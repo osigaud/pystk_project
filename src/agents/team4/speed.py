c1e1d17e537303850608e2a5c418a74cdf8a3778
ldgom <lucas.duarte.g75@gmail.com>
2026-03-29 12:03:50 +0200

    Compute_cuvature adaptation in SpeedController

--- src/agents/team4/speed.py
+++ src/agents/team4/speed.py
@@ -1,5 +1,6 @@
 from omegaconf import DictConfig
 import numpy as np
+from utils.track_utils import compute_curvature
 
 class SpeedController:
     
@@ -54,11 +55,20 @@ class SpeedController:
             return np.arccos(np.clip(np.dot(a, b), -1.0, 1.0))#np.dot correspond au produit scalaire
 
         k = (angle(v1,v2) + angle(v2,v3) + angle(v3,v4)) / 3
+        #print("angle renvoie ",k)
+
+        k2 = abs(compute_curvature(points[1:5][:3]))
+        #print("compute_curvature renvoie ", k2)
+
 
         v_target2 = self.amax/np.sqrt(1+self.g*k)
+        print('vitesse avec angle ',v_target2)
+
+        v_test = np.clip(self.amax/np.sqrt(1+k2),0, self.amax)
+        print('vitesse avec compute_curvature ', v_test)
 
         if v_target2 >= 0.96:  
             v_target2 = self.amax #suite a ce calcul v_target est limité a 0.96 donc ce if lui permet d'atteindre la vitesse maximale
         
-        return np.clip(v_target2,0,self.amax), False #on ajoute un gain 
+        return np.clip(v_test*1.75,0,self.amax), False #on ajoute un gain 
         
