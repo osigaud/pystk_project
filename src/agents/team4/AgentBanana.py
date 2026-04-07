375e5fffaa7c7ab9f81f6701e3dcea1e15ad4d7f
Adam <bouzou734@gmail.com>
2026-04-07 12:22:13 +0200

    after several tests, switching to an old version for AgentBanane without a rotation matrix

--- src/agents/team4/AgentBanana.py
+++ src/agents/team4/AgentBanana.py
@@ -39,30 +39,6 @@ class AgentBanana:
         self.pilotage.reset()
         self.use_corde = False
         
-    def rotate(self, x : float, z : float, angle : float) -> tuple[float,float]:
-        
-        """
-        Fonction permettant de faire la rotation de x et z selon un angle, formule basée sur la matrice de rotation
-        
-        Args:
-
-            x(float) : Décalage latéral de la cible.
-            z(float) : Profondeur de la cible.
-            angle(float) : Angle de rotation.
-
-        Returns:
-
-            float : Décalage latéral obtenu après rotation.
-            float : Profondeur obtenue après rotation.
-        """
-
-        cos_a = math.cos(angle)
-        sin_a = math.sin(angle)
-
-        x_prime = x*cos_a - z*sin_a
-        z_prime = x*sin_a + z*cos_a
-
-        return x_prime,z_prime
     
     def banana_detection(self,obs : dict,limit_path : float,center_path : float) -> tuple[str,float,list]:
         """
@@ -98,15 +74,12 @@ class AgentBanana:
                 pos_x = items_pos[i][0] # On récupère le décalage latéral 
                 pos_z = items_pos[i][2] # On récupère la profondeur
 
-                nx, nz = self.rotate(pos_x,pos_z,-angle_piste) # Rotation inverse pour ramener sur l'axe z
+                nx, nz = pos_x, pos_z
 
                 dist_obj_centre= abs(center_path+nx) #Calcul de la distance absolu de l'objet
 
                 if dist_obj_centre > limit_path: # Si l'objet est hors des limites de la piste, on ne le prend pas en compte
                     continue
-
-                """if abs(nx) > self.c.limite_danger_lateral:
-                    continue"""
 
                 if -self.c.radar_x <= nx <= self.c.radar_x and self.c.radar_zmin <= nz <= self.c.radar_zmax: # Si la banana est dans notre radar, on l'ajoute dans notre liste
                     banana.append((nx,nz))
@@ -124,18 +97,11 @@ class AgentBanana:
             second = banana[1] # On récupère la seconde
             x = second[0]
             z = second[1]
-            
-            # Si on capte un cas qui contredit l'hypothese d'une ligne, on renvoie SINGLE
-            for i in range(len(banana)):
-                for j in range(i+1,len(banana)):
-                    if abs(banana[i][1] - banana[j][1]) > self.c.seuil_ligne:
-                        banana.sort(key=lambda x : x[0])
-                        first = banana[0]
-                        first_x = first[0]
-                        return "SINGLE",first_x,banana
-                    
-            gap_x = (x+first_x)/2.0
-            return "LIGNE", gap_x, banana 
+            if abs(z-first_z) <= self.c.seuil_ligne: # Si les bananes forment une ligne (un barrage)
+                gap_x = (x+first_x)/2.0
+                return "LIGNE", gap_x, banana
+            else:
+                return "SINGLE", first_x,banana # On revient sur le cas de la banane seule
         
         else:
             return "SINGLE",first_x,banana # Cas d'une seule banane
@@ -184,34 +150,11 @@ class AgentBanana:
             self.lock_mode = None # On réinitialise l'état
             return False, {}
         
-        # Si la banane esseulée est trop loin de l'agent, on ne la regarde pass
-        if mode == "SINGLE" and abs(b_x) >= self.c.limite_banane_single and self.lock_mode != "LIGNE":
-            if self.dodge_timer <= 0:
-                return False, {}
         
         if mode == "SINGLE" and self.lock_mode != "LIGNE": # Si on a capte un cas d'une banane seule et qu'on était pas déjà dans une situation d'esquive de barrage
 
             #print(banana_list)
-            
-            # Prendre l'interieur des virages sur des virages pas trop serrés
-            """if abs(center_path_distance) <= self.c.limite_centre and abs(b_x) <= self.c.limite_banane_courbe and abs(courbe) <= self.c.limite_courbe:
-                self.use_corde = True
-                #print(courbe)
-                if -courbe >= self.c.true_virage: # Seuleument si la courbe tourne assez pour eviter l'instabilité
-                    #print("VIRAGE A DROITE")
-                    new_side = 1
-                elif courbe <= -self.c.true_virage:
-                    #print("VIRAGE A GAUCHE")
-                    new_side = -1
-                else:
-                    self.use_corde = False
-                    if b_x>=0:
-                        new_side = -1
-                    else:
-                        new_side = 1
-
-            else:"""
-                #print("choix normal")
+            #print("choix normal")
             
             self.use_corde = False
             if b_x>=0:
